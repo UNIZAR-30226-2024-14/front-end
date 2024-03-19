47d75f044c24b6c312f42eced7bad653929a8408
iaydemirankabeta <theangelo112@gmail.com>
2024-03-19 14:34:24 +0100

    Added logo and signup screen
    Signup screen and logo features added.

--- PokerBlackJackUI/src/app/components/signup/signup.component.ts
+++ PokerBlackJackUI/src/app/components/signup/signup.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss', '../../../styles.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
