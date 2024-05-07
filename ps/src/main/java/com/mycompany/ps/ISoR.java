4f78d5ee627cc3f53ce7ba12d4ab865f1f422794
jaimichuVG <124771913+jaimichuVG@users.noreply.github.com>
2024-05-07 17:12:42 +0200

    awddawdawd

--- ps/src/main/java/com/mycompany/ps/ISoR.java
+++ ps/src/main/java/com/mycompany/ps/ISoR.java
@@ -3,8 +3,22 @@ package com.mycompany.ps;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
+import javafx.scene.layout.VBox;
 
 public class ISoR {
+    
+    @FXML
+    private VBox vboxMenu;
+    
+    public void initialize() {
+        // Crear una animación de fundido (fade-in) para el VBox
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), vboxMenu);
+        fadeIn.setFromValue(0); // Comienza desde invisible
+        fadeIn.setToValue(1);   // Hasta completamente visible
+        fadeIn.play(); // Inicia la animación
+    }
     
     @FXML
     private void iniciarSesion() throws IOException {
