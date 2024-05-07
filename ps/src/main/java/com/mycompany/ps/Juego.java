615bc170846ddf3f831c10e3e3fdc78911046354
Lukita5 <104981121+Lukita5@users.noreply.github.com>
2024-05-07 18:12:50 +0200

    fondo verde

--- ps/src/main/java/com/mycompany/ps/Juego.java
+++ ps/src/main/java/com/mycompany/ps/Juego.java
@@ -1,12 +1,27 @@
 package com.mycompany.ps;
 
-import java.io.IOException;
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 public class Juego {
+    
+    @FXML
+    private VBox vbox; // Referencia al VBox
 
+    @FXML
+    private Button comienzo; // Referencia al botón
+
+    @FXML
+    private void comenzarPartida() {
+        // Cambiar el fondo a verde
+        vbox.setStyle("-fx-background-color: green;");
+
+        // Ocultar el botón
+        comienzo.setVisible(false);
+    }
+    
     @FXML
     private void exitApplication() {
         Platform.exit();
