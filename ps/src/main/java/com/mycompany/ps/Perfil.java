74719f94c175bac399a60885bc53c715b89dbf26
jaimichuVG <124771913+jaimichuVG@users.noreply.github.com>
2024-05-14 18:53:06 +0200

    CAMBIOS casi definitivos
    awdadw

--- ps/src/main/java/com/mycompany/ps/Perfil.java
+++ ps/src/main/java/com/mycompany/ps/Perfil.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import com.mycompany.ps.JuegoLocal;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class Perfil {
 
@@ -13,9 +17,27 @@ public class Perfil {
 
     @FXML
     private Label emailLabel;
+    
+    @FXML
+    private ImageView animationImageView;
 
     @FXML
     public void initialize() {
+        // Cargar la imagen de la animación
+        Image animationImage = new Image(getClass().getResourceAsStream("/images/coin3.gif"));
+        
+        // Establecer la imagen en el ImageView
+        animationImageView.setImage(animationImage);
+        
+        // Crear una transición de traducción para mover la imagen horizontalmente
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(5), animationImageView);
+        translateTransition.setFromX(-60); // Posición inicial en x
+        translateTransition.setFromY(0); // Posición inicial en x
+        //translateTransition.setToX(0); // Posición final en x
+        translateTransition.setCycleCount(TranslateTransition.INDEFINITE); // Repetir indefinidamente
+        translateTransition.setAutoReverse(true); // Revertir automáticamente
+        translateTransition.play(); // Iniciar la animación
+    
         try {
             // Obtener el nombre de usuario y correo electrónico utilizando las funciones de Auth.java
             // String token = "tu_token_de_autenticacion"; // Reemplaza esto con tu token de autenticación real
@@ -39,6 +61,11 @@ public class Perfil {
     @FXML
     public void cambiar2(){
         JuegoLocal.cambiarSkin("cards2");
+    }
+    
+    @FXML
+    public void cambiar3(){
+        JuegoLocal.cambiarSkin("cards3");
     }
     
     @FXML
